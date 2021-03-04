61a61917d719b53b2646cdce5adbe36d3160ca0d
devarias <gedafu2005@gmail.com>
2021-03-04 12:40:03 -0500

    first commit

---
+++ Main.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = 5000;
+
+app.use(cors());
+
+app.get('/api/v1/info/mentors', (req, res) => {
+  res.json({ name: 'Dev' });
+});
+app.get('/api/v1/info/companies', (req, res) => {
+  res.json({ name: 'Umbrella' });
+});
+
+app.listen(port, () => {
+  console.log(`listening on ${port}`);
+});
