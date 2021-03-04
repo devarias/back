9205a6543493303382f7ba344cae1673dfbf32ec
devarias <gedafu2005@gmail.com>
2021-03-04 13:25:14 -0500

    home route

--- Main.js
+++ Main.js
@@ -5,6 +5,10 @@ const port = 5000;
 
 app.use(cors());
 
+app.get('/', (req, res) => {
+  res.json({ name: 'Home' });
+});
+
 app.get('/api/v1/info/mentors', (req, res) => {
   res.json({ name: 'Dev' });
 });
